db512cee6d407ecbbabfbbf4e1177a295648c556
Glenn Trigg <ggtrigg@gmail.com>
2020-07-01 20:01:47 +1000

    Ensure client & dog creation works properly.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -21,6 +21,7 @@ class ClientsController < ApplicationController
   
   def create
     @client = Client.new(client_params)
+    @client.dogs.first.age_will_change! if @client.dogs.present?
  
     if @client.save
       redirect_to @client
@@ -50,12 +51,13 @@ class ClientsController < ApplicationController
   
   private
   def client_params
+    params['client']['dogs_attributes']['services'].delete_if(&:empty?) if params['client']['dogs_attributes']['services'].present?
     params.require(:client).permit(:firstname, :surname, :phone_number, :email_address, :documents,
       address_attributes: [:id, :street1, :street2, :suburb, :postcode, :state, :country],
-      dogs_attributes: [:id, :name, :breed, :age, :sex, :desexed],
-      notes_attributes: [:id, :annotation],
-      attendees_attributes: [:price, :status],
-      events_attributes: [:id, :event_type, :location, :price, :date, :duration])
+      dogs_attributes: [:id, :name, :breed, :age, :sex, :desexed, services: []],
+      notes_attributes: [:id, :annotation])
+      # attendees_attributes: [:price, :status],
+      # events_attributes: [:id, :event_type, :location, :price, :date, :duration])
   end
 
   def set_client
