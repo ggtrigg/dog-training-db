6db7a5963d60e2ab5a84ec531308df5c9ad83294
Glenn Trigg <ggtrigg@gmail.com>
2020-09-22 15:22:56 +1000

    Work in progress.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[trusona]
+
+  def self.from_omniauth(auth)
+    binding.pry
+  end
 end
